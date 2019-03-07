94bd9a1f08dfbcf7de6ea9bbf045d508df35edd2
chriselrod <elrodc@gmail.com>
2019-03-06 19:05:41 -0600

    updates to the tracked dict.

--- src/tracked_dict.jl
+++ src/tracked_dict.jl
@@ -1,5 +1,17 @@
 
-struct TrackedDict{K,V} <: AbstractDict{K,V}
-    d::Dict{K,V}
+struct TrackedDict{K,V,D<:AbstractDict{K,V}} <: AbstractDict{K,V}
+    d::D
+    oldd::D
+    reassigned::Dict{K,V}
+end
+function TrackedDict(d::AbstractDict{K,V}) where {K,V}
+    TrackedDict(d, copy(d), Dict{K,V}())
+end
 
+Base.haskey(td::TrackedDict, k) = haskey(td.d, k)
+Base.get(td::TrackedDict, k, v)  = get(td.d, k, v)
+Base.getindex(td::TrackedDict, k) = td.d[k]
+function Base.setindex!(td::TrackedDict, v, k)
+    haskey(oldd, k) && (td.reassigned[k] = td.d[k])
+    td.d[k] = v
 end
